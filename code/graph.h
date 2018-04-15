452318e6e6d96e24b9bd0fbbdb1e63d5106f7cf6
David Zeng <davidzeng.42@gmail.com>
2018-04-15 18:48:38 -0400

    Some work done precomputation

--- code/graph.h
+++ code/graph.h
@@ -6,14 +6,18 @@
 
 class Graph
 {
-  private:
+  public:
     int nnode;
     int nedge;
+
     int *neighbor;
     int *neighbor_start;
 
-  public:
     Graph(FILE *infile);
+    Graph(int nnode); // Need to constuct graph yourself
     int* get_neighbors(int node_id);
     int num_neighbors(int node_id);
+    int get_num_nodes();
 };
+
+#endif
--- code/uysp.h
+++ code/uysp.h
@@ -1,8 +1,19 @@
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+#include "graph.h"
+
 class UYSP {
-  int test;
+
+  int rho;
+  Graph *g;
+  int *hop_node_list;
+  int *hop_node_flag;
+  int *hop_graph;
 
   public:
-    UYSP(int a);
+    UYSP(Graph *g, int rho);
     int doPrecomputation();
     int query(int a, int b);
 };
