c841a8c82415adaf798470b3116198ad5a4bb6f9
Vincent Kang <vkang@andrew.cmu.edu>
2018-04-15 17:15:31 -0400

    Graph header

---
+++ code/graph.h
@@ -0,0 +1,19 @@
+// Graph.h
+#include <stdio.h>
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
+class Graph
+{
+  private:
+    int nnode;
+    int nedge;
+    int *neighbor;
+    int *neighbor_start;
+
+  public:
+    Graph(FILE *infile);
+    int* get_neighbors(int node_id);
+    int num_neighbors(int node_id);
+};
