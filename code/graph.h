cad290e5b06d4a41f23ffca344f7e86c6eef12bb
David Zeng <davidzeng.42@gmail.com>
2018-04-15 19:10:18 -0400

    minor

--- code/graph.h
+++ code/graph.h
@@ -14,7 +14,6 @@ class Graph
     int *neighbor_start;
 
     Graph(FILE *infile);
-    Graph(int nnode); // Need to constuct graph yourself
     int* get_neighbors(int node_id);
     int num_neighbors(int node_id);
     int get_num_nodes();
